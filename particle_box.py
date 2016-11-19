b20f2dbab406db57e8ce40419231c41f1ec9e7fd
Vikas Kurapati <vikaskurapati@iitb.ac.in>
2016-11-19 07:22:07 +0530

    Changed particles in a box accordingly involving functions.

--- particle_box.py
+++ particle_box.py
@@ -56,47 +56,53 @@ class ParticleBox:
         self.state[crossed_x1 | crossed_x2, 2] *= -1
         self.state[crossed_y1 | crossed_y2, 3] *= -1
         self.state[:, 3] -= self.M * self.G * dt
+
 np.random.seed(0)
-init_state = -0.5 + np.random.random((50, 4))
-init_state[:, :2] *= 3.9
-
-box = ParticleBox(init_state, size=0.04)
-dt = 1. / 30
-
-fig = plt.figure()
-fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
-                     xlim=(-3.2, 3.2), ylim=(-2.4, 2.4))
-
-particles, = ax.plot([], [], 'bo', ms=6)
-
-rect = plt.Rectangle(box.bounds[::2],
-                     box.bounds[1] - box.bounds[0],
-                     box.bounds[3] - box.bounds[2],
-                     ec='none', lw=2, fc='none')
-ax.add_patch(rect)
 
 
-def init():
-    global box, rect
-    particles.set_data([], [])
-    rect.set_edgecolor('none')
-    return particles, rect
+def Particles(dt=1./30, init_state=-0.5 + np.random.random((50, 4))):
+    init_state[:, :2] *= 3.9
+    box = ParticleBox(init_state, size=0.04)
+    return init_state, box
 
 
-def animate(i):
-    global box, rect, dt, ax, fig
-    box.step(dt)
+def animate(name, dt=1./30, init_state=-0.5 + np.random.random((50, 4))):
+    init_state, box = Particles(dt, init_state)
+    fig = plt.figure()
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
+                         xlim=(-3.2, 3.2), ylim=(-2.4, 2.4))
 
-    ms = int(fig.dpi * 2 * box.size * fig.get_figwidth() /
-             np.diff(ax.get_xbound())[0])
-    rect.set_edgecolor('k')
-    particles.set_data(box.state[:, 0], box.state[:, 1])
-    particles.set_markersize(ms)
-    return particles, rect
+    particles, = ax.plot([], [], 'bo', ms=6)
 
-ani = animation.FuncAnimation(fig, animate, frames=600,
-                              interval=10, blit=True, init_func=init)
-ani.save('particle_box.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+    rect = plt.Rectangle(box.bounds[::2],
+                         box.bounds[1] - box.bounds[0],
+                         box.bounds[3] - box.bounds[2],
+                         ec='none', lw=2, fc='none')
+    ax.add_patch(rect)
 
-plt.show()
+    def init():
+        # global box, rect
+        particles.set_data([], [])
+        rect.set_edgecolor('none')
+        return particles, rect
+
+    def animate(i):
+        # global box, rect, dt, ax, fig
+        box.step(dt)
+
+        ms = int(fig.dpi * 2 * box.size * fig.get_figwidth() /
+                 np.diff(ax.get_xbound())[0])
+        rect.set_edgecolor('k')
+        particles.set_data(box.state[:, 0], box.state[:, 1])
+        particles.set_markersize(ms)
+        return particles, rect
+
+    ani = animation.FuncAnimation(fig, animate, frames=600,
+                                  interval=10, blit=True, init_func=init)
+    ani.save(name+'.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+
+    plt.show()
+
+if __name__ == '__main__':
+    animate('particle_box')
