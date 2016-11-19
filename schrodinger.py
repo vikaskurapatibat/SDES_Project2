1f91e8321e1b2b510382687b2de9635d7a8a1336
Vikas Kurapati <vikaskurapati@iitb.ac.in>
2016-11-19 07:29:19 +0000

    Changed the schrodinger code accordingly incorporating functions

--- schrodinger.py
+++ schrodinger.py
@@ -107,87 +107,85 @@ def theta(x):
 def square_barrier(x, width, height):
     return height * (theta(x) - theta(x - width))
 
-dt = 0.01
-N_steps = 50
-t_max = 120
-frames = int(t_max / float(N_steps * dt))
+
 hbar = 1.0
-m = 1.9
-N = 2 ** 11
-dx = 0.1
-x = dx * (np.arange(N) - 0.5 * N)
-V0 = 1.5
-L = hbar / np.sqrt(2 * m * V0)
-a = 3 * L
-x0 = -60 * L
-V_x = square_barrier(x, a, V0)
-V_x[x < -98] = 1E6
-V_x[x > 98] = 1E6
-p0 = np.sqrt(2 * m * 0.2 * V0)
-dp2 = p0 * p0 * 1./80
-d = hbar / np.sqrt(2 * dp2)
-
-k0 = p0 / hbar
-v0 = p0 / m
-psi_x0 = gauss_x(x, d, x0, k0)
-S = Schrodinger(x=x,
-                psi_x0=psi_x0,
-                V_x=V_x,
-                hbar=hbar,
-                m=m,
-                k0=-28)
-fig = plt.figure()
-xlim = (-100, 100)
-klim = (-5, 5)
-ymin = 0
-ymax = V0
-ax1 = fig.add_subplot(211, xlim=xlim,
-                      ylim=(ymin - 0.2 * (ymax - ymin),
-                            ymax + 0.2 * (ymax - ymin)))
-psi_x_line, = ax1.plot([], [], c='r', label=r'$|\psi(x)|$')
-V_x_line, = ax1.plot([], [], c='k', label=r'$V(x)$')
-center_line = ax1.axvline(0, c='k', ls=':',
-                          label=r"$x_0 + v_0t$")
-title = ax1.set_title("")
-ax1.legend(prop=dict(size=12))
-ax1.set_xlabel('$x$')
-ax1.set_ylabel(r'$|\psi(x)|$')
-ymin = abs(S.psi_k).min()
-ymax = abs(S.psi_k).max()
-ax2 = fig.add_subplot(212, xlim=klim,
-                      ylim=(ymin - 0.2 * (ymax - ymin),
-                            ymax + 0.2 * (ymax - ymin)))
-psi_k_line, = ax2.plot([], [], c='r', label=r'$|\psi(k)|$')
-p0_line1 = ax2.axvline(-p0 / hbar, c='k', ls=':', label=r'$\pm p_0$')
-p0_line2 = ax2.axvline(p0 / hbar, c='k', ls=':')
-mV_line = ax2.axvline(np.sqrt(2 * V0) / hbar, c='k', ls='--',
-                      label=r'$\sqrt{2mV_0}$')
-ax2.legend(prop=dict(size=12))
-ax2.set_xlabel('$k$')
-ax2.set_ylabel(r'$|\psi(k)|$')
-V_x_line.set_data(S.x, S.V_x)
 
 
-def init():
-    psi_x_line.set_data([], [])
-    V_x_line.set_data([], [])
-    center_line.set_data([], [])
-    psi_k_line.set_data([], [])
-    title.set_text("")
-    return (psi_x_line, V_x_line, center_line, psi_k_line, title)
+def animate(name, dt=0.01, N_steps=50, t_max=120, m=1.9, N=2 ** 11, dx=0.1, V0=1.5):
+    frames = int(t_max / float(N_steps * dt))
+    x = dx * (np.arange(N) - 0.5 * N)
+    L = hbar / np.sqrt(2 * m * V0)
+    a = 3 * L
+    x0 = -60 * L
+    V_x = square_barrier(x, a, V0)
+    V_x[x < -98] = 1E6
+    V_x[x > 98] = 1E6
+    p0 = np.sqrt(2 * m * 0.2 * V0)
+    dp2 = p0 * p0 * 1./80
+    d = hbar / np.sqrt(2 * dp2)
 
-
-def animate(i):
-    S.time_step(dt, N_steps)
-    psi_x_line.set_data(S.x, 4 * abs(S.psi_x))
+    k0 = p0 / hbar
+    v0 = p0 / m
+    psi_x0 = gauss_x(x, d, x0, k0)
+    S = Schrodinger(x=x,
+                    psi_x0=psi_x0,
+                    V_x=V_x,
+                    hbar=hbar,
+                    m=m,
+                    k0=-28)
+    fig = plt.figure()
+    xlim = (-100, 100)
+    klim = (-5, 5)
+    ymin = 0
+    ymax = V0
+    ax1 = fig.add_subplot(211, xlim=xlim,
+                          ylim=(ymin - 0.2 * (ymax - ymin),
+                                ymax + 0.2 * (ymax - ymin)))
+    psi_x_line, = ax1.plot([], [], c='r', label=r'$|\psi(x)|$')
+    V_x_line, = ax1.plot([], [], c='k', label=r'$V(x)$')
+    center_line = ax1.axvline(0, c='k', ls=':',
+                              label=r"$x_0 + v_0t$")
+    title = ax1.set_title("")
+    ax1.legend(prop=dict(size=12))
+    ax1.set_xlabel('$x$')
+    ax1.set_ylabel(r'$|\psi(x)|$')
+    ymin = abs(S.psi_k).min()
+    ymax = abs(S.psi_k).max()
+    ax2 = fig.add_subplot(212, xlim=klim,
+                          ylim=(ymin - 0.2 * (ymax - ymin),
+                                ymax + 0.2 * (ymax - ymin)))
+    psi_k_line, = ax2.plot([], [], c='r', label=r'$|\psi(k)|$')
+    p0_line1 = ax2.axvline(-p0 / hbar, c='k', ls=':', label=r'$\pm p_0$')
+    p0_line2 = ax2.axvline(p0 / hbar, c='k', ls=':')
+    mV_line = ax2.axvline(np.sqrt(2 * V0) / hbar, c='k', ls='--',
+                          label=r'$\sqrt{2mV_0}$')
+    ax2.legend(prop=dict(size=12))
+    ax2.set_xlabel('$k$')
+    ax2.set_ylabel(r'$|\psi(k)|$')
     V_x_line.set_data(S.x, S.V_x)
-    center_line.set_data(2 * [x0 + S.t * p0 / m], [0, 1])
-    psi_k_line.set_data(S.k, abs(S.psi_k))
-    title.set_text("t = %.2f" % S.t)
-    return (psi_x_line, V_x_line, center_line, psi_k_line, title)
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=frames, interval=30, blit=True)
 
-anim.save('schrodinger.mp4', fps=15, extra_args=['-vcodec', 'libx264'])
+    def init():
+        psi_x_line.set_data([], [])
+        V_x_line.set_data([], [])
+        center_line.set_data([], [])
+        psi_k_line.set_data([], [])
+        title.set_text("")
+        return (psi_x_line, V_x_line, center_line, psi_k_line, title)
 
-plt.show()
+    def animate(i):
+        S.time_step(dt, N_steps)
+        psi_x_line.set_data(S.x, 4 * abs(S.psi_x))
+        V_x_line.set_data(S.x, S.V_x)
+        center_line.set_data(2 * [x0 + S.t * p0 / m], [0, 1])
+        psi_k_line.set_data(S.k, abs(S.psi_k))
+        title.set_text("t = %.2f" % S.t)
+        return (psi_x_line, V_x_line, center_line, psi_k_line, title)
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=frames, interval=30, blit=True)
+
+    anim.save('schrodinger.mp4', fps=15, extra_args=['-vcodec', 'libx264'])
+
+    plt.show()
+
+if __name__ == '__main__':
+    animate('schrodinger')
